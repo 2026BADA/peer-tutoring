5fd9ec17e637a25fc3483e1e8dc9d663bc1b18d9
Carmel1215 <kth12153@gmail.com>
2026-05-19 23:18:59 +0900

    feat: prettier 추가

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <nav className="flex items-center justify-between p-4 bg-white shadow">
-            <Link href="/" className="font-bold text-lg">
+        <nav className="flex items-center justify-between bg-white p-4 shadow">
+            <Link href="/" className="text-lg font-bold">
                 Peer Tutoring (임시)
             </Link>
             <div className="flex gap-4">
                 <Link href="/questions">질문</Link>
-                <Link href="/profile/me">프로필</Link>{" "}
                 {/* 나중에 프로필 동적 라우팅 수정하기 */}
+                <Link href="/profile/me">프로필</Link>
             </div>
         </nav>
     );
