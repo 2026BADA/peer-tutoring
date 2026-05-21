46f8627a818e9a7f440e60ac89492ed1c0cd48de
Carmel1215 <kth12153@gmail.com>
2026-05-21 22:32:04 +0900

    feat: middleware 추가

---
+++ middleware.ts
@@ -0,0 +1,47 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+    let supabaseResponse = NextResponse.next({ request });
+
+    // 서버 클라이언트 생성
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                getAll() {
+                    return request.cookies.getAll();
+                },
+                setAll(cookiesToSet) {
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        request.cookies.set(name, value),
+                    );
+                    supabaseResponse = NextResponse.next({ request });
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        supabaseResponse.cookies.set(name, value, options),
+                    );
+                },
+            },
+        },
+    );
+
+    // 유저 받아오기
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    // 유저가 아닐 경우 login으로 리디렉션
+    if (!user && !request.nextUrl.pathname.startsWith("/login")) {
+        const url = request.nextUrl.clone();
+        url.pathname = "/login";
+        return NextResponse.redirect(url);
+    }
+
+    return supabaseResponse;
+}
+
+// 미들웨어가 실행되지 않아야 하는 경로
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login).*)"],
+};
