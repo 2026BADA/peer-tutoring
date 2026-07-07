22ba8c6767f8a7d493913da279f1fb20d6ac761b
Carmel1215 <kth12153@gmail.com>
2026-07-08 00:05:00 +0900

    feat: Google OAuth 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,11 @@
+import { login } from "./actions";
+
 export default function Login() {
-    return <div></div>;
+    return (
+        <div className="flex flex-1 items-center justify-center">
+            <form action={login}>
+                <button className="btn btn-neutral">Google로 로그인</button>
+            </form>
+        </div>
+    );
 }
