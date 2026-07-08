b011b6a3229b33b064793d4415ea7b10e269f6c6
Carmel1215 <kth12153@gmail.com>
2026-07-08 14:04:02 +0900

    feat: 로그아웃 추가
    Navbar에 로그아웃 버튼 및 분기 추가

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,7 +1,13 @@
-// 모든 페이지 상단에 존재하는 네비게이션 바
 import Link from "next/link";
+import { createClient } from "@/lib/supabase/server";
+import { signOut } from "@/app/actions";
 
-export default function NavBar() {
+export default async function NavBar() {
+    const supabase = await createClient();
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
     return (
         <nav className="flex items-center justify-between bg-white p-4 shadow">
             <Link href="/" className="text-lg font-bold">
@@ -9,11 +15,19 @@ export default function NavBar() {
             </Link>
             <div className="flex gap-4">
                 <Link href="/questions">질문</Link>
-                {/* TODO: 프로필 동적 라우팅 수정하기 */}
-                <Link href="/profile/me">프로필</Link>
-                <Link href="/login">로그인</Link>
-                {/* TODO: 로그인 한 경우 로그아웃으로 바꾸기 */}
+                {/* NOTE: 로그인 버튼 동적 라우팅을 위한 처리 */}
+                {user ? (
+                    <>
+                        <Link href={`/profile/${user.id}`}>프로필</Link>
+                        <form action={signOut}>
+                            <button type="submit">로그아웃</button>
+                        </form>
+                    </>
+                ) : (
+                    <Link href="/login">로그인</Link>
+                )}
             </div>
+            ㅎ{" "}
         </nav>
     );
 }
